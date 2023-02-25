f7c7a0142491e7b4ec0ac3995fa19eb9d5219eca
prateek041 <prateeksingh9741@gmail.com>
2023-02-25 13:22:11 +0530

    Added middleware to authenticate JWT

--- server/src/permissions/rules.js
+++ server/src/permissions/rules.js
@@ -1,9 +1,22 @@
-const {rule} = require("graphql-shield")
+const { rule } = require("graphql-shield")
+const { verifyToken } = require("../utils/hashingUtil")
 
-const isAuthenticated = rule()(
-  async (parent, args, context)=>{
-    return true;
+const isAuthenticated = rule({ cache: 'contextual' })(
+  async (_, args, context) => {
+    console.log("This function was run")
+    const token = context.token?.split(" ")[1];
+    if (token !== null) {
+      const user = await verifyToken(token)
+      // Fetch the user info and attach to context.
+      if (user !== null) { // if the token is correct, attach info to the context.
+        context.user = user;
+        return true;
+      }
+      return false;
+    }
+    console.log("There was an error")
+    return false; //
   }
 )
 
-module.exports = {isAuthenticated}
+module.exports = { isAuthenticated }
